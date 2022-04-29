725d779d5e668a2784ead38b5fa8df5e15e3d0cd
Romashkina <da_shootka@mail.ru>
2022-04-29 17:27:53 +0200

    feat: implement decision for queue

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,19 +1,12 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
 * using Node from extensions
 */
 
-// class Node {
-//   constructor(value) {
-//     this.value = value;
-//     this.left = null;
-//     this.right = null;
-//   }
-// }
 
 
 
