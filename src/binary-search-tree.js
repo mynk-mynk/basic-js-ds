c549ec05d1e9167cb70667aca4dccb5be10de046
Romashkina <da_shootka@mail.ru>
2022-04-29 16:42:08 +0200

    feat: implement decision for stack

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,151 +7,154 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 
-class Node {
-  constructor(value) {
-    this.value = value;
-    this.left = null;
-    this.right = null;
-  }
-}
+// class Node {
+//   constructor(value) {
+//     this.value = value;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
 
 
 class BinarySearchTree {
-
+  
   constructor() {
-    this.content = null;
+    throw new NotImplementedError('Not implemented');
+    // this.content = null;
   }
+  
+  // root() {
+  //   this.root.data = this.value ? this.value : null;
+  //   return this.root.data;
+  // }
 
-  root() {
-    this.root.data = this.value ? this.value : null;
-    return this.root.data;
-  }
+  // add(data) {
 
-  add(data) {
+  //   this.content = addNode(this.content, data);
 
-    this.content = addNode(this.content, data);
+  //   function addNode(node, value) {
 
-    function addNode(node, value) {
+  //     if (!node) {
+  //       return new Node(value);
+  //     }
 
-      if (!node) {
-        return new Node(value);
-      }
+  //     if (node.value === value) {
+  //       return node;
 
-      if (node.value === value) {
-        return node;
+  //     } else {
 
-      } else {
+  //       if (value < node.value) node.left = addNode(node.left, value);
+  //       if (value > node.value) node.right = addNode(node.right, value);
+  //     }
 
-        if (value < node.value) node.left = addNode(node.left, value);
-        if (value > node.value) node.right = addNode(node.right, value);
-      }
+  //     return node;
+  //   }
+  // }
 
-      return node;
-    }
-  }
+  // has(data) {
+  //   return hasNode(this.content, data);
 
-  has(data) {
-    return hasNode(this.content, data);
+  //   function hasNode(node, value) {
 
-    function hasNode(node, value) {
+  //     if (!node) return false;
 
-      if (!node) return false;
+  //     if (node.value === value) {
+  //       return true;
+  //     }
 
-      if (node.value === value) {
-        return true;
-      }
+  //     return value < node.value ? hasNode(node.left, value) : hasNode(node.right, value);
+  //   }
+  // }
 
-      return value < node.value ? hasNode(node.left, value) : hasNode(node.right, value);
-    }
-  }
+  // find(data) {
+  //   this.find.data = null;
+  //   return findNode(this.content, data);
 
-  find(data) {
-    return findNode(this.content, data);
+  //   function findNode(node, value) {
 
-    function findNode(node, value) {
+  //     if (!node) return null;
 
-      if (!node) return null;
+  //     if (node.value === value) {
+  //       return node;
+  //     }
 
-      if (node.value === value) {
-        return node;
-      }
+  //     return value < node.value ? findNode(node.left, value) : findNode(node.right, value);
+  //   }
+  // }
 
-      return value < node.value ? findNode(node.left, value) : findNode(node.right, value);
-    }
-  }
+  // remove(data) {
+  //   this.content = removeNode(this.content, data);
 
-  remove(data) {
-    this.content = removeNode(this.content, data);
+  //   function removeNode(node, value) {
 
-    function removeNode(node, value) {
+  //     if (!node) return;
 
-      if (!node) return;
+  //     if (value < node.value) {
+  //       node.left = removeNode(node.left, value);
+  //       return node;
 
-      if (value < node.value) {
-        node.left = removeNode(node.left, value);
-        return node;
+  //     } else if (value > node.value) {
+  //       node.right = removeNode(node.right, value);
+  //       return node;
 
-      } else if (value > node.value) {
-        node.right = removeNode(node.right, value);
-        return node;
+  //     } else {
 
-      } else {
+  //       if (!node.left && !node.right) return null;
+  //     }
 
-        if (!node.left && !node.right) return null;
-      }
+  //     if (!node.left) {
+  //       node = node.right;
+  //       return node;
+  //     }
 
-      if (!node.left) {
-        node = node.right;
-        return node;
-      }
+  //     if (!node.right) {
+  //       node = node.left;
+  //       return node;
+  //     }
 
-      if (!node.right) {
-        node = node.left;
-        return node;
-      }
+  //     let maxLeft = node.left;
 
-      let maxLeft = node.left;
+  //     while (maxLeft.right) {
+  //       maxLeft = maxLeft.right;
+  //     }
 
-      while (maxLeft.right) {
-        maxLeft = maxLeft.right;
-      }
+  //     node.value = maxLeft.value;
+  //     node.left = removeNode(node.left, maxLeft.value);
 
-      node.value = maxLeft.value;
-      node.left = removeNode(node.left, maxLeft.value);
+  //     return node;
+  //   }
+  // }
 
-      return node;
-    }
-  }
+  // min() {
+  //   return findMin(this.content);
 
-  min() {
-    return findMin(this.content);
+  //   function findMin(node) {
 
-    function findMin(node) {
+  //     if (!node) return;
 
-      if (!node) return;
+  //     while (node.left) {
+  //       node = node.left;
+  //     }
 
-      while (node.left) {
-        node = node.left;
-      }
+  //     return node.value;
+  //   }
+  // }
 
-      return node.value;
-    }
-  }
+  // max() {
+  //   return findMax(this.content);
 
-  max() {
-    return findMax(this.content);
+  //   function findMax(node) {
 
-    function findMax(node) {
+  //     if (!node) return;
 
-      if (!node) return;
+  //     while (node.right) {
+  //       node = node.right;
+  //     }
 
-      while (node.right) {
-        node = node.right;
-      }
-
-      return node.value;
-    }
-  }
+  //     return node.value;
+  //   }
+  // }
 }
 
 module.exports = {
--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -13,18 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  * @example
  * For l = [3, 1, 2, 3, 4, 5] and k = 3,
  * the output should be [1, 2, 4, 5]
- *
- * Singly - linked lists are already defined using interface
- * class ListNode {
- *   constructor(x) {
- *     this.value = x;
- *     this.next = null;
- *   }
- * }
+ Singly - linked lists are already defined using interface
  */
-function removeKFromList(/* l, k */) {
+
+// class ListNode {
+//   constructor(x) {
+//     this.value = x;
+//     this.next = null;
+//   }
+// }
+
+
+function removeKFromList(l, k) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+
+  // let list = l;
+  // let listItem = list;
+
+  // while (listItem) {
+
+  //   if (listItem.value === k) {
+  //     this = this.next;
+  //     listItem = listItem.next;
+    
+  //   } else {
+  //     listItem = listItem.next;
+  //   }
+
+  // }
+
 }
 
 module.exports = {
